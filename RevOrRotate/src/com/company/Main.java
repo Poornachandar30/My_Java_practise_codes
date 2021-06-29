364d8a79c7006be22ea3de238d5259dfb8dd4635
Poornachandar30 <30531994+Poornachandar30@users.noreply.github.com>
2021-06-29 19:01:55 +0530

    new

--- RevOrRotate/src/com/company/Main.java
+++ RevOrRotate/src/com/company/Main.java
@@ -9,40 +9,44 @@ public class Main {
         System.out.println(revRot("733049910872815764",5));
     }
     public static String revRot(String strng, int sz) {
-        if((sz==0)||(sz>strng.length())||(strng=="")){
+        System.out.println("strng " + strng);
+        System.out.println("sz "+sz);
+        if((sz==0)||(sz>strng.length())||(strng.equals(""))){
             return "";
         }
         else{
-             int length = 0;
-             String substring="";
-             length = strng.length()-(strng.length() % sz);
-            substring = strng.substring(0,length);
-            int len = substring.length();
+            int length = 0;
+            String subs="";
+            String revOrRotate ="";
+            length = strng.length()-(strng.length() % sz);
+            subs = strng.substring(0,length);
+            int len = subs.length();
             int k=0;
-            String part[]= new String[sz];
+            String part[]= new String[length/sz];
             for(int j=0; j<len; j+=sz){
-                part[k] = substring.substring(j,j+sz);
+                part[k] = subs.substring(j,Math.min(j,j+sz));
                 k=k+1;
             }
-            for(int i=0; i<= part.length; i++){
-                if(part[i]==null){
-                    break;
-                }
+            for(int i=0; i< part.length; i++){
                 String split[]=part[i].split("");
                 int[] chunks = new int[split.length];
                 int digitCube = 0;
-                 String revOrRotate ="";
                 for(int j=0; j< split.length; j++){
                     chunks[j] = Integer.parseInt(split[j]);
-                    digitCube=chunks[j]^3+digitCube;
+                    digitCube=(int)Math.pow(chunks[j],3)+digitCube;
                 }
                 if(digitCube%2==0){
-
+                    StringBuilder sb =new StringBuilder(part[i]);
+                    revOrRotate = revOrRotate+sb.reverse().toString();
+                }else {
+                    String shifting = part[i];
+                    shifting = shifting.substring(1,sz) + Character.toString(part[i].charAt(0));
+                    revOrRotate = revOrRotate + shifting;
                 }
+                System.out.println(Arrays.toString(part));
                 System.out.println(Arrays.toString(chunks));
             }
-            System.out.println(Arrays.toString(part));
-            return "";
+            return revOrRotate;
         }
     }
 }
